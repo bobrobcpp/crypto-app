5590fa5eac72d65a2ceab160017e430c8726a6b8
bobrobcpp <king.roob@gmail.com>
2018-02-15 14:36:21 +0000

    Add currency-symbol data and description to column and dropdown menu

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 (function() {
+  //Get symbol map object from currency-symbol.js
+  const symbolMap = CURRENCIES;
   let currencies = [];
   let source = $('#some-template').html();
   let template = Handlebars.compile(source);
-  let rates = [];
+  let rates = [{ name: 'USD', rate: 1, desc: 'US Dollars' }];
   let currentRate;
   $(document).on('click', 'footer', function() {
     $('footer').hide();
@@ -13,17 +15,20 @@
     ratesObj = rateData.rates;
 
     for (prop in ratesObj) {
-      rates.push({ name: prop, rate: ratesObj[prop] });
+      rates.push({ name: prop, rate: ratesObj[prop], desc: symbolMap[prop].name });
     }
   })
     .then(() => {
       //Load the current cryptocurrency prices in USD and create dataO object with the data and adding the rates array for handlebars compilation
       $.getJSON('https://api.coinmarketcap.com/v1/ticker/?limit=10', function(data) {
         dataO = { currencies: data, rates: rates };
-        dataO.currentRate;
+        dataO.currentRate = 'USD';
+        dataO.currentSymbol = '$';
+        dataO.currentName = 'US Dollars';
         $(document).on('click', '#optionText', function() {
           dataO.currentRate = this.value;
-          console.log(dataO);
+          dataO.currentSymbol = symbolMap[this.value].symbol;
+          console.log('dataO', dataO);
           $('#content-placeholder').html(template(dataO));
         });
         $('#content-placeholder').html(template(dataO));
@@ -34,7 +39,7 @@
       //Handlebars helper to multiply the crytocurrency price with the exchange rate
       Handlebars.registerHelper('formatPrice', function(USDPrice, current) {
         if (current) {
-          var currObj = rates.find(function(obj) {
+          let currObj = rates.find(function(obj) {
             return obj.name === current;
           });
 
